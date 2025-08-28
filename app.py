5f5226a0d08d68a684f3353b0df6ccec3038f31d
Shashira Ravishka <ravishkashashira@gmail.com>
2025-08-28 17:37:12 +0530

    app.py updated

--- app.py
+++ app.py
@@ -361,44 +361,6 @@ def profile():
     return render_template('profile.html')
 
 
-@app.route('/index', methods=['POST'])
-def handle_post():
-    if 'uid' not in session:
-        return redirect(url_for('login'))
-
-    text = request.form['text']
-    logging.info(f'Text : {text}')
-
-    preprocessed_txt = preprocessing(text)
-    logging.info(f'Preprocessed Text : {preprocessed_txt}')
-
-    vectorized_txt = vectorizer(preprocessed_txt)
-    logging.info(f'Vectorized Text : {vectorized_txt}')
-
-    prediction = get_prediction(vectorized_txt)
-    logging.info(f'Prediction : {prediction}')
-
-    # New variables for just current review
-    if prediction == 'fake':
-        real_count = 0
-        fake_count = 1
-    else:
-        real_count = 1
-        fake_count = 0
-
-    total = real_count + fake_count
-    real_percent = round((real_count / total) * 100)
-    fake_percent = round((fake_count / total) * 100)
-
-    data = {
-        'reviews': [text],
-        'real': real_count,
-        'fake': fake_count,
-        'real_percent': real_percent,
-        'fake_percent': fake_percent
-    }
-
-    return render_template('index.html', data=data)
 
 if __name__ == '__main__':
     app.run()
